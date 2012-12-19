0cf4a8903ea98a9244472ac98a7851d5d66623b4
Raymond Chan <raycchan@gmail.com>
2012-12-19 15:03:23 -0500

    added comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,11 @@
 class Photo < ActiveRecord::Base
   validates :photo_url, :uniqueness => true
 
-  def get_images(celeb_name)
-    celeb_name = celeb_name.downcase.gsub(' ','-')
-    seed_url = "http://www.thesuperficial.com/tag/#{celeb_name}/news"
+  def get_images(urls) #takes array of urls
+    # celeb_name = celeb_name.downcase.gsub(' ','-')
+    # seed_url = "http://www.thesuperficial.com/tag/#{celeb_name}/news"
+  end
+
+  def get_urls(depth, seed_url) #returns array of urls
   end
 end
