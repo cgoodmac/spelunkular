30112f1fdfb786886522cd1f1ea4e7299200bd83
Dustin Coates <dustin@dcoates.com>
2012-12-19 15:55:08 -0500

    Filled out get_urls method more

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -1,27 +1,41 @@
 class Crawler
 
 def get_images(urls) #takes array of urls
-    regex_img=/<img .*?src.*?['"]([^'"]*).*?>\w*/m
+  regex_img=/<img .*?src.*?['"]([^'"]*).*?>\w*/m
 
+  urls.each do |url|
+    #.scan returns an array of arrays with length 1
+    url.scan(regex_img).each{|pic| Photo.create(:photo_url => pic[0])}
+  end
+end
+
+def get_urls(depth, seed_url) #returns array of urls
+  urls = [seed_url]
+  new_urls = []
+  regex_url = /<a .*?href.*?['"]([^'"]+)['"]>/m
+
+  while depth > 0
+    #Getting the images from each URL we crawled.
+    get_images(urls)
+
+    #Crawling each URL we were given.
     urls.each do |url|
-      #.scan returns an array of arrays with length 1
-      url.scan(regex_img).each{|pic| Photo.create(:photo_url => pic[0])}
+      new_urls << HTTParty.get(url).scan(regex_url)
     end
-  end
 
-  def get_urls(depth, seed_url) #returns array of urls
-    urls = [seed_url]
-    regex_url = /<a .*?href.*?['"]([^'"]+)['"]>/m
+    #Clearing this so we don't double crawl.
+    urls = []
 
-    while depth > 0
-      get_images(urls)
-
-      urls.scan(regex_url).each do |url|
-        urls << URI.parase(url).relative? ? "http://www.usmagazine.com" + url : url
-      end
-
-      depth -= 1
+    #Check to see if we have an asolute URL.
+    new_urls.each do |url|
+      urls << URI.parse(url).relative? ? "http://www.usmagazine.com" + url : url
     end
+
+    #Clearing out this new array so we don't double crawl.
+    new_urls = []
+
+    depth -= 1
   end
+end
 
 end
