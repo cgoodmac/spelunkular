e3c7ef452637436d662b0a6f645cbf0808322e58
Raymond Chan <raycchan@gmail.com>
2012-12-19 15:51:40 -0500

    fixed get_images function

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121219193612) do
+
+  create_table "photos", :force => true do |t|
+    t.string   "photo_url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
